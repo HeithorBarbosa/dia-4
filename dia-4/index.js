1c0478853a960d260a15c24f5da21418684a6023
Heithor Barbosa <heithor.barbosa321@gmail.com>
2024-09-03 19:05:01 -0300

    dia-4 ex1

--- dia-4/index.js
+++ dia-4/index.js
@@ -1,10 +1,11 @@
-let name = prompt("digite seu nome:");
-let idade = Number(prompt("qual é a sua idade ?"));
+let fome = prompt("você está com fome?");
+let dinheiro = Number(prompt("você tem dinheiro ?"));
+let restaurante = prompt("o restaurante está aberto hoje ?");
 
-if (name === "heithor" && idade === 17) {
-  console.log("você se chama heithor e tem 17 anos");
-} else if (name === "heithor" || idade === 17) {
-  console.log("seu nome é heithor OU você tem 17 anos");
+if (fome === "não" || dinheiro === "não") {
+  console.log("Hoje a janta será em casa");
+} else if (fome === "sim" && dinheiro === "sim" && restaurante === "não") {
+  console.log("Peca um delivery");
 } else {
-  console.log("você é um impostor");
+  console.log("vamos no seu restaurante preferido");
 }
